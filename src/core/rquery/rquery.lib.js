9555bf55e0e1e7d8e162f53ab7a63f5e5218d23d
Maxim Gudkov <maksimop6@gmail.com>
2023-12-23 02:02:57 +0500

    add new methods in rquery lib

--- src/core/rquery/rquery.lib.js
+++ src/core/rquery/rquery.lib.js
@@ -1,3 +1,5 @@
+import { formatCardNumberWithDashes } from '@/utils/format/format-card-number'
+
 /**
  * Represents the RQuery class for working with DOM elements.
  */
@@ -20,6 +22,8 @@ class RQuery {
 		}
 	}
 
+	/* FIND */
+
 	/**
 	 * Find the first element that matches the specified selector within the selected element.
 	 * @param {string} selector - A CSS selector string to search for within the selected element.
@@ -34,6 +38,8 @@ class RQuery {
 			throw new Error(`Element ${selector} not found!`)
 		}
 	}
+
+	/* INSERT */
 
 	/**
 	 * Append a new element as a child of the selected element.
@@ -79,6 +85,87 @@ class RQuery {
 		}
 	}
 
+	/* EVENTS */
+
+	/**
+	 * Attach a click event listener to the selected element.
+	 * @param {function(Event): void} callback - The event listener function to execute when the selected element is clicked. The function will receive the event object as its argument.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	click(callback) {
+		this.element.addEventListener('click', callback)
+		return this
+	}
+
+	/* FORM */
+
+	/**
+	 * Set attributes and event listeners for an input element.
+	 * @param {object} options - An object containing input options.
+	 * @param {function(Event): void} [options.onInput] - The event listener for the input's input event.
+	 * @param {object} [options.rest] - Optional attributes to set on the input element.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	input({ onInput, ...rest }) {
+		if (this.element.tagName.toLowerCase() !== 'input')
+			throw new Error('Element must be an input')
+
+		for (const [key, value] of Object.entries(rest)) {
+			this.element.setAttribute(key, value)
+		}
+
+		if (onInput) {
+			this.element.addEventListener('input', onInput)
+		}
+
+		return this
+	}
+
+	/**
+	 * Set attributes and event listeners for a number input element.
+	 * @param {number} [limit] - The maximum length of input value.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	numberInput(limit) {
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'number'
+		)
+			throw new Error('Element must be an input with type "number"')
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = value
+		})
+
+		return this
+	}
+
+	/**
+	 * Set attributes and event listeners for a credit card input element.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	creditCardInput() {
+		const limit = 16
+
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'text'
+		)
+			throw new Error('Element must be an input with type "text"')
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = formatCardNumberWithDashes(value)
+		})
+
+		return this
+	}
+
+	/* STYLES */
+
 	/**
 	 * Set the CSS style of the selected element.
 	 * @param {string} property - The CSS property to set.
@@ -91,6 +178,40 @@ class RQuery {
 		}
 
 		this.element.style[property] = value
+		return this
+	}
+
+	/**
+	 * Adds a class or a list of classes to the current element.
+	 * @param {string | string[]} classNames - A single class name or an array of class names to add to the element.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	addClass(classNames) {
+		if (Array.isArray(classNames)) {
+			for (const className of classNames) {
+				this.element.classList.add(className)
+			}
+		} else {
+			this.element.classList.add(classNames)
+		}
+
+		return this
+	}
+
+	/**
+	 * Removes a class or a list of classes from the current element.
+	 * @param {string | string[]} classNames - A single class name or an array of class names to remove from the element.
+	 * @returns {RQuery} The current RQuery instance for chaining.
+	 */
+	removeClass(classNames) {
+		if (Array.isArray(classNames)) {
+			for (const className of classNames) {
+				this.element.classList.remove(className)
+			}
+		} else {
+			this.element.classList.remove(classNames)
+		}
+
 		return this
 	}
 }
