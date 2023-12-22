b56105ae745560b1bf8a49a9e3db4223583ff5fd
Maxim Gudkov <maksimop6@gmail.com>
2023-12-23 02:08:48 +0500

    test field and input ui components

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -2,6 +2,9 @@ import { BaseScreen } from '@/core/component/base-screen.component'
 import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
 
+import { Field } from '@/components/ui/field/field.component'
+import { Button } from '@/components/ui/button/button.component'
+
 import styles from './home.module.scss'
 import template from './home.template.html'
 
@@ -11,7 +14,18 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(template, [], styles)
+		const element = renderService.htmlToElement(
+			template,
+			[
+				new Field({
+					name: 'mwerg',
+					placeholder: 'Enter email',
+					variant: 'green'
+				}),
+				new Button({children: 'ok', onClick: () => console.log('ok'), variant: 'purple'})
+			],
+			styles
+		)
 
 		$R(element).find('h1').css('color', 'green')
 
