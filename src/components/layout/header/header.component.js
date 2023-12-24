408d857fc2def15a3c3ca523cb0f91ee4c955028
Maxim Gudkov <maksimop6@gmail.com>
2023-12-24 23:46:03 +0500

    update header component

--- src/components/layout/header/header.component.js
+++ src/components/layout/header/header.component.js
@@ -1,12 +1,38 @@
 import ChildComponent from '@/core/component/child.component'
 import renderService from '@/core/services/render.service'
 
+import { UserItem } from '@/components/ui/user-item/user-item.component'
+
 import styles from './header.module.scss'
 import template from './header.template.html'
 
+import { Logo } from './logo/logo.component'
+import { LogoutButton } from './logout-button/logout-button.component'
+import { Search } from './search/search.component'
+
 export class Header extends ChildComponent {
+	constructor({ router }) {
+		super()
+		this.router = router
+	}
+
 	render() {
-		this.element = renderService.htmlToElement(template, [], styles)
+		this.element = renderService.htmlToElement(
+			template,
+			[
+				Logo,
+				new LogoutButton({
+					router: this.router
+				}),
+				Search,
+				new UserItem({
+					avatarPath:
+						'https://prisma-blog-ebon.vercel.app/blog/posts/type-safe_js_with_JsDoc.png',
+					name: 'Max'
+				})
+			],
+			styles
+		)
 
 		return this.element
 	}
