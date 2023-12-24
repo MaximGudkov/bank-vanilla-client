113d0b34b4edfb4b58a91cd592de130262e854b5
Maxim Gudkov <maksimop6@gmail.com>
2023-12-24 23:41:33 +0500

    clean home component

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,9 +1,7 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
-import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
 
 import { Field } from '@/components/ui/field/field.component'
-import { Button } from '@/components/ui/button/button.component'
 
 import styles from './home.module.scss'
 import template from './home.template.html'
@@ -21,13 +19,10 @@ export class Home extends BaseScreen {
 					name: 'mwerg',
 					placeholder: 'Enter email',
 					variant: 'green'
-				}),
-				new Button({children: 'ok', onClick: () => console.log('ok'), variant: 'purple'})
+				})
 			],
 			styles
 		)
-
-		$R(element).find('h1').css('color', 'green')
 
 		return element
 	}
