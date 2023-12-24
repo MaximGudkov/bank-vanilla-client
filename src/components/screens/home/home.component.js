38972af394d11f220bb8d9fdbd76c3fbb905f02f
Maxim Gudkov <maksimop6@gmail.com>
2023-12-24 23:46:25 +0500

    clean

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,8 +1,6 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
 import renderService from '@/core/services/render.service'
 
-import { Field } from '@/components/ui/field/field.component'
-
 import styles from './home.module.scss'
 import template from './home.template.html'
 
@@ -12,17 +10,7 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(
-			template,
-			[
-				new Field({
-					name: 'mwerg',
-					placeholder: 'Enter email',
-					variant: 'green'
-				})
-			],
-			styles
-		)
+		const element = renderService.htmlToElement(template, [], styles)
 
 		return element
 	}
